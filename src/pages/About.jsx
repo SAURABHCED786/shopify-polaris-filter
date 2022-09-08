beeef4e63efbf0e677e679dbf2ee65a7a98271f0
saurabhced786 <saurabhgupta@cedcommerce.com>
2022-09-08 18:24:37 +0530

    Modified

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h2>This is About Us Page</h2>
+      <h2>This Site Showing Data of GitHub Users</h2>
+      <p>You can enter the user name of GitHub API and show the Result. </p>
+      <br />
+      <br />
+      <p>API Url is https://api.github.com/users</p>
+      <br />
+      <p><b>Here You Can See The Users Details.!</b></p>
     </div>
   )
 }
